adea66d8bca6b9fd4aea0cf2f440bdd144ed3760
Akinyele Ritchie <157580464+AkinyeleRitchie@users.noreply.github.com>
2025-10-25 16:16:33 -0800

    Remove unnecessary blank line in Email_Signup.py

--- Email_Signup.py
+++ Email_Signup.py
@@ -18,7 +18,6 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
-
 # ------------------------------------------
 # Database setup
 # ------------------------------------------
